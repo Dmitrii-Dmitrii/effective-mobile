a85e915b9b45fab84d2abb6ebc69974948cdf4dd
Dmitrii Onshin <dmitrii.onshin@mail.ru>
2025-05-17 14:14:58 +0300

    add model person and migration

---
+++ internal/models/person.go
@@ -0,0 +1,20 @@
+package models
+
+import "github.com/jackc/pgx/v5/pgtype"
+
+type Person struct {
+	id         pgtype.UUID
+	name       string
+	surname    string
+	patronymic string
+	age        uint32
+	gender     string
+	country    string
+}
+
+type GenderType string
+
+const (
+	male   GenderType = "male"
+	female GenderType = "female"
+)
